965179f7ffc5cfbf66e65795823e31b837289e35
Memokre <147373468+Memokre@users.noreply.github.com>
2024-01-10 11:54:11 +0000

    ok

--- TKCodes/prehodeniehodnot/xd.cpp
+++ TKCodes/prehodeniehodnot/xd.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
     int a = 1;
     int b = 0;
-    cout << a<< "   " << b;
-   
-    int value;
+    cout << a << "   " << b;
 
-    value = a;  //1
-    a = b;   // a = 00
-    b = value;
+    int* aPtr = &a;
+    int* bPtr = &b;
 
+    int temp = *aPtr; 
+    *aPtr = *bPtr;   
+    *bPtr = temp;     
 
     cout << " \n";
     cout << a << "   " << b;
